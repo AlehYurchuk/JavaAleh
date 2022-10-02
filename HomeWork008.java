7625786ca8b0b3ccd886d34f48f7877711eaa383
AlehYurchuk <112678223+AlehYurchuk@users.noreply.github.com>
2022-10-02 17:46:51 +0200

    Add files via upload

---
+++ HomeWork008.java
@@ -0,0 +1,21 @@
+/**
+ *  Java Basic. Home work007
+ *
+ * @author Aleh
+ * @TODO: 28.09.2022
+ * @date 02.10.2022
+ */
+public class HomeWork008 {
+    public static void main(String[] args) {
+        Cat cat = Cat ("Bro", "red", 2, 1);
+        System.out.println(cat);
+        System.out.println("Try to play:" + cat.play());
+        cat.feed();
+        System.out.println("Try to play" + cat.play());
+        System.out.println("Try to play" + cat.play());
+        cat.feed();
+        System.out.println("Try to jump:" + cat.jump(1));
+        cat.feed();
+        System.out.println("Try to jump:" + cat.jump(2));
+    }
+}
